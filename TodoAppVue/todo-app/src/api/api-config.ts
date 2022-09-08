aa00321870b98c0891323ad1bd51dbea3e94dcb9
Jorge Gutiérrez <jorgegutierrezdev94@gmail.com>
2022-09-08 14:23:40 +0200

    docker-compose v1

--- TodoAppVue/todo-app/src/api/api-config.ts
+++ TodoAppVue/todo-app/src/api/api-config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const debug = process.env.NODE_ENV !== "production";
-const baseURL = debug ? "https://localhost:7161/api/" : "";
+//const debug = process.env.NODE_ENV !== "production";
+const baseURL = "http://localhost:5000/api/";
 const api = axios.create({ baseURL });
 export default api;
